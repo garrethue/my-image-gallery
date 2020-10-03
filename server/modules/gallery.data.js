1d3ab18117cce67434663153ddffcfbea7d50a14
Garret Larson <garret.hs.larson@gmail.com>
2020-10-03 16:17:52 -0500

    added more images.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -2,7 +2,7 @@ const galleryItems = [
   {
     id: 1,
     path: "images/library.jpeg",
-    description: "I got to tour the Library of Trinity College Dublin.",
+    description: "I toured the Library of Trinity College Dublin.",
     likes: 0,
   },
   {
@@ -14,7 +14,8 @@ const galleryItems = [
   {
     id: 3,
     path: "images/coachella3.jpeg",
-    description: "Me doing what everybody does at Coachella.",
+    description:
+      "Me doing what everybody does at Coachella: posing for those pics.",
     likes: 0,
   },
   {
@@ -28,7 +29,7 @@ const galleryItems = [
     id: 5,
     path: "images/sf.jpeg",
     description:
-      "New Year's day in San Francisco. I ran about 15 miles that day around SF, touring the city. I had an out-of-body experience running over the Golden Gate Bridge.",
+      "New Year's day in San Francisco (2019). I ran about 15 miles that day around SF, touring the city. I had an out-of-body experience running over the Golden Gate Bridge.",
     likes: 0,
   },
   {
@@ -36,6 +37,55 @@ const galleryItems = [
     path: "images/medora.jpeg",
     description:
       "Me doing yoga in Medora. Fun fact: The official Medora instagram requested to use this image for advertising campaigns.",
+    likes: 0,
+  },
+  {
+    id: 7,
+    path: "images/MississippiHike.jpeg",
+    description:
+      "Some of my close friends I met in Mississippi went on a hike/tour around the Vicksburg Military Park. It was gorgeous.",
+    likes: 0,
+  },
+  {
+    id: 8,
+    path: "images/paige-and-me.jpeg",
+    description:
+      "A close friend of mine and me in Grand Forks, ND, on New Year's Eve 2017.",
+    likes: 0,
+  },
+  {
+    id: 9,
+    path: "images/proof1.jpeg",
+    description:
+      "Part 1 of 2 of a mathematical proof I did a couple weeks back. Fun fact: I like to learn about mathematics and its Computer Science applications",
+    likes: 0,
+  },
+  {
+    id: 10,
+    path: "images/proof2.jpeg",
+    description:
+      "Part 2 of 2 of a mathematical proof I did a couple weeks back.",
+    likes: 0,
+  },
+  {
+    id: 11,
+    path: "images/vienna-palace.jpeg",
+    description:
+      "I went for a 13-mile run around the Schönbrunn Palace in Vienna, Austria. Google it. It's beautiful!",
+    likes: 0,
+  },
+  {
+    id: 12,
+    path: "images/blackmetalgarret.jpeg",
+    description:
+      "Halloween 2019 I dressed up of a version of me if I was into black/death metal.",
+    likes: 0,
+  },
+  {
+    id: 13,
+    path: "images/bday.jpeg",
+    description:
+      "Me on my 27th birthday. I went to Chicago for a music festival and to see a close friend. I saw one of my favorite musical artists, Robyn.",
     likes: 0,
   },
 ];
