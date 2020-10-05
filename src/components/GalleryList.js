7ba88aa6e7d75d2efdd2d68dda0f9ff8dab2f51d
Garret Larson <garret.hs.larson@gmail.com>
2020-10-05 08:48:58 -0500

    Added Material-ui card component

--- src/components/GalleryList.js
+++ src/components/GalleryList.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
 import GalleryItem from "./GalleryItem";
+import ImageCard from "./ImageCard.js";
 
 export default function GalleryList(props) {
   return (
     <div className="wrapper">
-      {props.imageItems.map((imgObj) => (
-        <GalleryItem
-          updateLike={props.updateLike}
-          deleteItem={props.deleteItem}
-          key={imgObj.id}
-          imgObj={imgObj}
-        />
-      ))}
+      {props.imageItems.map((imgObj) => {
+        return (
+          <Fragment>
+            {/* <GalleryItem
+              updateLike={props.updateLike}
+              deleteItem={props.deleteItem}
+              key={imgObj.id}
+              imgObj={imgObj}
+            /> */}
+            <ImageCard
+              updateLike={props.updateLike}
+              deleteItem={props.deleteItem}
+              key={imgObj.id}
+              imgObj={imgObj}
+            />
+          </Fragment>
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/ImageCard.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 350,
+  },
+  media: {
+    height: 240,
+  },
+});
+
+export default function MediaCard(props) {
+  const classes = useStyles();
+  console.log(props);
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.imgObj.path}
+          title={props.imgObj.description}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.imgObj.description}
+            <div className="number-of-likes">Likes: {props.imgObj.likes}</div>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button
+          onClick={() => props.deleteItem(props.imgObj.id)}
+          size="small"
+          color="primary"
+        >
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            class="bi bi-trash-fill"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"
+            />
+          </svg>
+        </Button>
+        <Button
+          onClick={(event) =>
+            props.updateLike(props.imgObj.id, props.imgObj.likes)
+          }
+          size="small"
+          color="primary"
+        >
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            className="bi bi-heart-fill"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+            />
+          </svg>
+        </Button>
+        <a href={props.imgObj.path} className="img-1">
+          <Button size="small" color="primary">
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              className="bi bi-arrows-fullscreen"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z"
+              />
+            </svg>
+          </Button>
+        </a>
+      </CardActions>
+    </Card>
+  );
+}
