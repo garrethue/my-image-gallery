ffa04a0526612a388d427115c9a6b2bd9faa20bf
Garret Larson <garret.hs.larson@gmail.com>
2020-10-05 19:07:55 -0500

    Added some more photos and modified the about page.

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,12 @@ export default function About() {
     <div className="about">
       <h1>Hi!</h1>
       <h1>My name is Garret Larson.</h1>
+      <br />
       <h3>Welcome to my gallery.</h3>
+      <h3>
+        I am a NoDak native who has a passion for exercise, travel, and
+        programming.
+      </h3>
     </div>
   );
 }
